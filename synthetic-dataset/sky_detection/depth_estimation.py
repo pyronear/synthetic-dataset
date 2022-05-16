1b51c90e86f63ba39a77f9b5f908c721f8e0a026
Josselin Pennors <josselin.pennors@hotmail.fr>
2022-05-16 23:48:52 +0200

    Module Refactoring

--- synthetic-dataset/sky_detection/depth_estimation.py
+++ synthetic-dataset/sky_detection/depth_estimation.py
@@ -1,42 +1,102 @@
 import torch
 
-# Depth Deep Learning Model
-model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
-#model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
-#model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+class DepthEstimation:
 
-midas = torch.hub.load("intel-isl/MiDaS", model_type)
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-midas.to(device)
-midas.eval()
+    # Depth Deep Learning Model
+    MIDAS_LARGE = "DPT_Large" # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+    MIDAS_HYBRID = "DPT_Hybrid" # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+    MIDAS_SMALL = "MiDaS_small" # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
 
-midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+    def __init__(self, model_type = MIDAS_LARGE):
 
-if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
-    transform = midas_transforms.dpt_transform
-else:
-    transform = midas_transforms.small_transform
+        # Preloading model
+        midas = torch.hub.load("intel-isl/MiDaS", model_type)
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        midas.to(device)
+        midas.eval()
 
-def get_depth_from_color(color_img):
-    """ Get depth image from midas DL model """
-    input_batch = transform(color_img).to(device)
-    with torch.no_grad():
-        prediction = midas(input_batch)
-        prediction = torch.nn.functional.interpolate(
-            prediction.unsqueeze(1),
-            size=color_img.shape[:2],
-            mode="bicubic",
-            align_corners=False,
-        ).squeeze()
+        midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
 
-    output = prediction.cpu().numpy()
-    output = (output/40*255).astype(int) # Here is to scale the range of value to approach 255
-    return output
+        if model_type == MIDAS_LARGE or model_type == MIDAS_HYBRID:
+            transform = midas_transforms.dpt_transform
+        else:
+            transform = midas_transforms.small_transform
 
-def masking_uninterested(depth_image, threshold_min = 60, threshold_max = 170):
-    """ Masking the sky (and too closed object) by threshold based on empirical testing """
-    threshold_min = 65
-    threshold_max = 170
-    depth_image[depth_image < threshold_min] = 0
-    depth_image[depth_image > threshold_max] = 0
-    return depth_image
+    def detect_sky_from_depth(img, min_threshold = 60, max_threshold = 170):
+        """ 
+            Method to estimate the depth from an image
+            and automatically generate the mask from it
+            to remove the sky pixels
+
+            Args:
+                img (np.array): background image
+                min_threshold (int): min pixel value not to be considered as sky
+                max_threshold (int): max pixel value not to be considered as sky
+
+            Returns:
+                np.array: result mask image
+        """
+
+        depth_image = estimate_depth_from_image(img)
+        
+        return generate_mask(depth_image, min_threshold, max_threshold)
+
+    def estimate_depth_from_image(img):
+        """
+            Estimate the depth the image pixel by pixel with Torch
+
+            Args:
+                img (np.array): background image
+
+            Returns:
+                np.array: result depth image
+        """
+        
+        input = transform(img).to(device)
+
+        with torch.no_grad():
+            prediction = midas(input)
+            prediction = torch.nn.functional.interpolate(
+                prediction.unsqueeze(1),
+                size=img.shape[:2],
+                mode="bicubic",
+                align_corners=False,
+            ).squeeze()
+
+        output = prediction.cpu().numpy()
+
+        return _scale_output(output)
+
+
+    def generate_mask(depth_image, min_threshold, max_threshold):
+        """ 
+            Generate a mask without the sky
+            This methods relies on empirical tests
+
+            Args:
+                img (np.array): background image
+                min_threshold (int): min pixel value not to be considered as sky
+                max_threshold (int): max pixel value not to be considered as sky
+
+            Returns:
+                np.array: result mask image
+        """
+
+        depth_image[depth_image < min_threshold] = 0
+        depth_image[depth_image > max_threshold] = 0
+        return depth_image
+
+    def _scale_output(output):
+        """
+            Scaling Depth Output Image
+            This methods relies on empirical tests
+            The maximum value of a single pixel is 40 and it needs to
+            to be scaled between 0 and 255
+
+            Args:
+                output (np.array): background image
+
+            Returns:
+                np.array: result scaled image
+        """
+        return (output/40*255).astype(int)
