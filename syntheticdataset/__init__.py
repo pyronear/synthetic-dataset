59ef295a0bd43186af0f292205918da433eb0025
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2022-05-17 23:05:07 +0200

    fix header

--- syntheticdataset/__init__.py
+++ syntheticdataset/__init__.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2021, Pyronear contributors.
+
+# This program is licensed under the GNU Affero General Public License version 3.
+# See LICENSE or go to <https://www.gnu.org/licenses/agpl-3.0.txt> for full license details.
+
 from .image_blending import *
 from .make_set import *
 from .poisson_blending_utils import *
