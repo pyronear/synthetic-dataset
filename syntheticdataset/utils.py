fe41871cfdcdf3cc6041d42a64a37d11742eb5cf
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2022-05-17 21:35:14 +0200

    rename

--- syntheticdataset/utils.py
+++ syntheticdataset/utils.py
@@ -8,23 +8,23 @@ def read_video(file):
     cap = cv2.VideoCapture(file)
     imgs = []
     ret = True
-    while(cap.isOpened() and ret):
+    while cap.isOpened() and ret:
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret:
             imgs.append(frame)
- 
+
     return imgs
 
 
 def save_img(folder_path, filename, img):
     os.makedirs(folder_path, exist_ok=True)
-    cv2.imwrite(f'{folder_path}/{filename}', img)
+    cv2.imwrite(f"{folder_path}/{filename}", img)
 
 
 def save_label(folder_path, filename, label):
     os.makedirs(folder_path, exist_ok=True)
-    with open(f'{folder_path}/{filename}', 'w') as f:
+    with open(f"{folder_path}/{filename}", "w") as f:
         f.write(label)
 
 
@@ -35,11 +35,11 @@ def get_label(mask):
         y0, x0 = np.min(np.where(mask), 1)
         y1, x1 = np.max(np.where(mask), 1)
         h, w = mask.shape
-        dw = (x1 - x0)/w
-        dh = (y1 - y0)/h
-        xc = x0/w + dw/2
-        yc = y0/h + dh/2
-        label = str(1) + ' ' + str(xc) + ' ' + str(yc) + ' ' + str(dw) + ' ' + str(dh)
+        dw = (x1 - x0) / w
+        dh = (y1 - y0) / h
+        xc = x0 / w + dw / 2
+        yc = y0 / h + dh / 2
+        label = str(1) + " " + str(xc) + " " + str(yc) + " " + str(dw) + " " + str(dh)
     else:
         label = ""
 
