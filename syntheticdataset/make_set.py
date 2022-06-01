808a5dadaacd51bdcc00c6d590ee7ed791d0ae07
Josselin Pennors <josselin.pennors@hotmail.fr>
2022-06-01 15:22:19 +0200

    Fix Depth Estimation

--- syntheticdataset/make_set.py
+++ syntheticdataset/make_set.py
@@ -8,7 +8,7 @@ import numpy as np
 import random
 from syntheticdataset.utils import read_video, save_img, save_label, get_label
 from syntheticdataset.image_blending import basic_blending, poisson_blending
-from syntheticdataset.randomization.randomization import get_random_start_point
+from syntheticdataset.randomization.randomization import Randomization
 
 
 BLENDING_METHODS = {
@@ -33,6 +33,8 @@ def make_one_set(
     size_max_bg=1280,
     size_max_smoke=1280,
 ):
+
+    randomization = Randomization()
 
     # Get smokes frames
     smoke_imgs = read_video(smoke_video_file, size_max=size_max_smoke)
@@ -64,10 +66,10 @@ def make_one_set(
     # Random offset
     hs, ws = smoke_imgs[0].shape[:2]
     hbg, wbg = imgs[0].shape[:2]
+    dx, dy = randomization.get_random_start_point(imgs[0])
 
-    if hs < hbg and ws < wbg:
-        dx, dy = get_random_start_point(smoke_imgs[0])
-
+    if hs < hbg and ws < wbg and dy > 0:
+    
         train_val = "train" if train else "val"
 
         for blending_type, blending_method in BLENDING_METHODS.items():
--- syntheticdataset/randomization/randomization.py
+++ syntheticdataset/randomization/randomization.py
@@ -3,33 +3,40 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-from syntheticdataset.randomization.depth_estimation import detect_sky_from_depth
+from syntheticdataset.randomization.depth_estimation import DepthEstimation
 import random
 import numpy as np
 
+class Randomization:
+    def __init__(self):
 
-def get_random_start_point(src_image):
-    """
-    This methods generates a random dx index as a start point for the smoke
-    and then leverages sky detection to better define which dy to choose
+        self.depth_estimation = DepthEstimation()
 
-    Args :
-        src_image (np.array) : source image
+    def get_random_start_point(self, src_image):
+        """
+        This methods generates a random dx index as a start point for the smoke
+        and then leverages sky detection to better define which dy to choose
 
-    Returns :
-        tuple start point of the smoke
-    """
+        Args :
+            src_image (np.array) : source image
 
-    # Define the dx value to start the smoke at
-    img_width = src_image.shape[1]
-    dx = random.randint(0, img_width - 1)
+        Returns :
+            tuple start point of the smoke
+        """
 
-    # Detect the sky in the source image
-    sky_mask = detect_sky_from_depth(src_image)
+        # Define the dx value to start the smoke at
+        img_width = src_image.shape[1]
+        dx = random.randint(0, img_width - 1)
 
-    # Search for the max dy value not to be in the sky at the dx index
-    max_dy = np.argmax(sky_mask[:, dx] == 0) - 1
-    min_dy = np.argmin(sky_mask[:, dx] == 0) - 1
-    dy = random.randint(min_dy, max_dy - 1)
+        # Detect the sky in the source image
+        sky_mask = self.depth_estimation.detect_sky_from_depth(src_image)
 
-    return dx, dy
+        # Search for the max dy value not to be in the sky at the dx index
+        max_dy = np.argmin(sky_mask[:, dx] == 0)
+
+        if(max_dy == 0 ):
+            return dx, 0
+
+        dy = random.randint(0, max_dy - 1)
+
+        return dx, dy
